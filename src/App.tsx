96049f8337009de7a2944e8c6ac8f34143e0d67c
saidulrizal <muhamadsaidulrizal@gmail.com>
2025-08-23 01:55:26 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 // import React from 'react';
-import StarField from './components/StarField';
-import Navigation from './components/Navigation';
-import Hero from './components/Hero';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import StarField from './components/StarField';
+// import Navigation from './components/Navigation';
+// import Hero from './components/Hero';
+// import About from './components/About';
+// import Portfolio from './components/Portfolio';
+// import Skills from './components/Skills';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
 import './App.css';
+import ComingSoon from './cooming-soon/page';
 
 function App() {
   return (
     <div className="relative min-h-screen overflow-x-hidden">
-      <StarField />
+      <ComingSoon/>
+      {/* <StarField />
       <Navigation />
       <main className="relative z-10">
         <Hero />
@@ -21,7 +23,7 @@ function App() {
         <Skills />
         <Contact />
       </main>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-
-
+"use client"
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-// import App from './App.tsx';
+// import ComingSoon from './cooming-soon/page';
 import './index.css';
-import ComingSoon from './cooming-soon/page';
+
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <ComingSoon/>
+    <App />
+    {/* <ComingSoon/> */}
   </StrictMode>
 );
