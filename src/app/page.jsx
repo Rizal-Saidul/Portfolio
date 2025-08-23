73b3500f2eb165816b7ce2c07193fae786abe09b
saidulrizal <muhamadsaidulrizal@gmail.com>
2025-08-23 12:16:25 +0700

    update

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ComingSoon from "./coming-soon/page";
+
+export default function Home() {
+  return (
+    <div className="0">
+      <ComingSoon/>
+    </div>
+  );
+}
